9a4ae7b3a153b74a0718e6891bc54477bc6479c8
Liyanamana Puhulwellage Hirushi Sathsarani Liyanamana <lphsliyanamana@gmail.com>
2024-10-18 19:29:09 +0530

    Update Task_02_V1.py

--- Task_02_V1.py
+++ Task_02_V1.py
@@ -3,7 +3,7 @@ Room_Door_State = {i: 'close' for i in range(1, n + 1)} #dictionary for room num
 print(Room_Door_State)
 open_count = 0
 close_count = 0
-
+#print (x, Room_Door_State)
 for x in range (1 , n+1):
     for j in range (x , n+1):
         if (j%x ==0):
@@ -11,8 +11,8 @@ for x in range (1 , n+1):
                 Room_Door_State[j] = 'close'
             else:
                 Room_Door_State[j] = 'open'
-    print (x, Room_Door_State)
-print (Room_Door_State)
+    #print (x, Room_Door_State)
+#print (Room_Door_State)
 
 for k in range (1,n+1):
     if (Room_Door_State[k] == 'open'):
